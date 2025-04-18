d527d2115ea7d436feb97c1ad0c9d699e5a80268
Dario <darioekinkela@gmail.com>
2025-04-18 11:16:00 -0300

    error forma de pago

--- src/main/java/com/dkinkel/gastos/model/forma/FormaPago.java
+++ src/main/java/com/dkinkel/gastos/model/forma/FormaPago.java
@@ -11,10 +11,7 @@ public class FormaPago {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(nullable = false)
-    private String nombre;
-
+  
     @Column(nullable = false)
     private String tipo;  // "EFECTIVO" o "TARJETA"
 
